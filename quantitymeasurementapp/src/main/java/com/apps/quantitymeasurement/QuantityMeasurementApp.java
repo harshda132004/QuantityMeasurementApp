0b3721da3d1c2d15f5496ca2eb888b1740b94d7a
Harshda132004 <pawarharshda058@gmail.com>
2026-02-08 23:19:53 +0530

    UC2: Feet and Inches measurement equality

--- quantitymeasurementapp/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ quantitymeasurementapp/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,11 +3,11 @@ package com.apps.quantitymeasurement;
 import java.util.Scanner;
 
 /**
- * QuantityMeasurementApp - UC1: Feet measurement equality
+ * QuantityMeasurementApp – UC2: Feet and Inches measurement equality
  */
 public class QuantityMeasurementApp {
 
-    // Inner class representing Feet
+    // Inner class to represent Feet measurement
     public static class Feet {
         private final double value;
 
@@ -15,48 +15,83 @@ public class QuantityMeasurementApp {
             this.value = value;
         }
 
-        /**
-         * Equality check for Feet objects
-         */
         @Override
         public boolean equals(Object obj) {
-
-            // 1. Reference check
             if (this == obj)
                 return true;
 
-            // 2. Null check
-            if (obj == null)
+            if (obj == null || this.getClass() != obj.getClass())
                 return false;
 
-            // 3. Type check
-            if (this.getClass() != obj.getClass())
-                return false;
-
-            // 4. Value comparison
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
         }
     }
 
-    // Main method to demonstrate Feet equality check
-    public static void main(String[] args) {
+    // Inner class to represent Inches measurement
+    public static class Inches {
+        private final double value;
 
-        Scanner scanner = new Scanner(System.in);
+        public Inches(double value) {
+            this.value = value;
+        }
 
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+
+            if (obj == null || this.getClass() != obj.getClass())
+                return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // Take user input and check Inches equality
+    public static void demonstrateInchesEquality(Scanner sc) {
+        System.out.print("Enter first value in inches: ");
+        double v1 = sc.nextDouble();
+
+        System.out.print("Enter second value in inches: ");
+        double v2 = sc.nextDouble();
+
+        Inches inch1 = new Inches(v1);
+        Inches inch2 = new Inches(v2);
+
+        boolean result = inch1.equals(inch2);
+
+        System.out.println("Input: " + v1 + " inch and " + v2 + " inch");
+        System.out.println("Output: Equal (" + result + ")");
+        System.out.println();
+    }
+
+    // Take user input and check Feet equality
+    public static void demonstrateFeetEquality(Scanner sc) {
         System.out.print("Enter first value in feet: ");
-        double firstValue = scanner.nextDouble();
+        double v1 = sc.nextDouble();
 
         System.out.print("Enter second value in feet: ");
-        double secondValue = scanner.nextDouble();
+        double v2 = sc.nextDouble();
 
-        Feet feet1 = new Feet(firstValue);
-        Feet feet2 = new Feet(secondValue);
+        Feet feet1 = new Feet(v1);
+        Feet feet2 = new Feet(v2);
 
         boolean result = feet1.equals(feet2);
 
-        System.out.println("Equal (" + result + ")");
+        System.out.println("Input: " + v1 + " ft and " + v2 + " ft");
+        System.out.println("Output: Equal (" + result + ")");
+        System.out.println();
+    }
 
-        scanner.close();
+    // Main method
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        demonstrateInchesEquality(sc);
+        demonstrateFeetEquality(sc);
+
+        sc.close();
     }
 }
